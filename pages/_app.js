9cb14266fd51b1166bac3aae6abf6db3f7a9f9a6
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-02-06 15:38:00 +0900

    polarisのスタイルシートをCDNにて使用する形へ変更

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import React from 'react';
-import '@shopify/polaris/dist/styles.css';
 import translations from '@shopify/polaris/locales/ja.json';
+// import '@shopify/polaris/dist/styles.css';
 
 class MyApp extends App {
   render() {
@@ -13,6 +13,10 @@ class MyApp extends App {
         <Head>
           <title>Sample App</title>
           <meta charSet="utf-8" />
+          <link
+            rel="stylesheet"
+            href="https://sdks.shopifycdn.com/polaris/3.18.0/polaris.min.css"
+          />
         </Head>
         <AppProvider i18n={translations}>
           <Component { ...pageProps } />
